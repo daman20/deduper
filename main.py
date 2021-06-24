a0a87e5658c21044f8fec7e4eb72a61170f89ae7
Aman Agrawal <79991838+daman20@users.noreply.github.com>
2021-06-24 15:41:20 +0000

    Added recursive dir functionality

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #Bundles the Helper scripts together
-import deletedupes, makehashes
-makehashes.main()
+import deletedupes, makehashes, os
+directory = "."
+for i in [x[0] for x in os.walk(directory)]:
+  makehashes.main(i)
 deletedupes.main()
--- makehashes.py
+++ makehashes.py
@@ -7,13 +7,13 @@ import json
 def sha512(fname):
     hash_sha = hashlib.sha512()
     if(fname != "hashes.txt"):
+      print(f"{fname} is being delted if necessary")
       with open(fname, "rb") as f:
           for chunk in iter(lambda: f.read(2 ** 20), b""):
               hash_sha.update(chunk)
     return hash_sha.hexdigest()
-def main():
+def main(mypath):
   hashes = {}
-  mypath = "/scan"
   filenames = [f for f in listdir(mypath) if isfile(join(mypath, f))]
   for filename in filenames:
     print(f"Now Calculating : {filename}")

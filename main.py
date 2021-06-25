1e74e1e242503195067acb37afd20ce1ae3065fb
Aman Agrawal <79991838+daman20@users.noreply.github.com>
2021-06-25 03:09:27 +0000

    Changed scan dir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Bundles the Helper scripts together
 import deletedupes, makehashes, os, json
-directory = "."
+directory = "/scan"
 allsubdirs = list_subfolders_with_paths = [f.path for f in os.scandir(directory) if f.is_dir()]
 for i in allsubdirs:
   print(i)

f083e994c12b65c4858bc08dd396b44bbad2cc96
daman12 <59244226+daman12@users.noreply.github.com>
2021-06-28 13:56:32 -0400

    changed how it gets all files

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 #Bundles the Helper scripts together
 import deletedupes, makehashes, os, json, guifunctions
+
+
+def getListOfFiles(dirName):
+  # create a list of file and sub directories
+  # names in the given directory
+  listOfFile = os.listdir(dirName)
+  allFiles = list()
+  # Iterate over all the entries
+  for entry in listOfFile:
+    # Create full path
+    fullPath = os.path.join(dirName, entry)
+    # If entry is a directory then get the list of files in this directory
+    if os.path.isdir(fullPath):
+      allFiles = allFiles + getListOfFiles(fullPath)
+    else:
+      allFiles.append(fullPath)
+
+  return allFiles
+
 if os.path.exists("/appdata/hashes.json"):
   os.remove("/appdata/hashes.json")
 directory = "/scan"
-allsubdirs = list_subfolders_with_paths = [f.path for f in os.scandir(directory) if f.is_dir()]
+allsubdirs = getListOfFiles(directory)
 guifunctions.oktextui(f"The program is currently hashing", "Hashing")
 for i in allsubdirs:
   print(i)

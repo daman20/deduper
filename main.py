3088d7c631082809804f2f8611cc81980d375712
daman12 <59244226+daman12@users.noreply.github.com>
2021-06-28 14:12:32 -0400

    added unlimited nested folder scanning

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
 #Bundles the Helper scripts together
 import deletedupes, makehashes, os, json, guifunctions
 
-
-def getListOfFiles(dirName):
-  # create a list of file and sub directories
-  # names in the given directory
-  listOfFile = os.listdir(dirName)
-  allFiles = list()
-  # Iterate over all the entries
-  for entry in listOfFile:
-    # Create full path
-    fullPath = os.path.join(dirName, entry)
-    # If entry is a directory then get the list of files in this directory
-    if os.path.isdir(fullPath):
-      allFiles = allFiles + getListOfFiles(fullPath)
-    else:
-      allFiles.append(fullPath)
-
-  return allFiles
-
 if os.path.exists("/appdata/hashes.json"):
   os.remove("/appdata/hashes.json")
 directory = "/scan"
-allfiles = getListOfFiles(directory)
 guifunctions.oktextui(f"The program is currently hashing", "Hashing")
-for i in allfiles:
-  print(i)
-  hashes = makehashes.main(i)
+hashes = makehashes.realmain(directory)
 with open("/appdata/hashes.json", "a+") as myfile:
   print(myfile)
   myfile.write(json.dumps(hashes))
--- makehashes.py
+++ makehashes.py
@@ -7,6 +7,23 @@ import json
 import guifunctions
 from guifunctions import *
 
+def getListOfFiles(dirName):
+  # create a list of file and sub directories
+  # names in the given directory
+  listOfFile = os.listdir(dirName)
+  allFiles = list()
+  # Iterate over all the entries
+  for entry in listOfFile:
+    # Create full path
+    fullPath = os.path.join(dirName, entry)
+    # If entry is a directory then get the list of files in this directory
+    if os.path.isdir(fullPath):
+      allFiles = allFiles + getListOfFiles(fullPath)
+    else:
+      allFiles.append(fullPath)
+
+  return allFiles
+
 def sha512(fname):
     hash_sha = hashlib.sha512()
     if(fname != "hashes.json"):
@@ -20,3 +37,5 @@ def main(filenames):
     print(f"Now Calculating : {filename}")
     hashes[filename] = sha512(filename)
   return hashes
+def realmain(directory):
+    return main(getListOfFiles(directory))
